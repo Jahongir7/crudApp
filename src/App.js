d45601d12734f4987d485269b66031ecf3eb7cda
Jahongir7 <Jahongir95119@gmail.com>
2021-06-28 15:11:16 +0500

    Alert and list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import { useState } from "react";
-import List from "./components/list";
+import List from "./components/List";
+import Alert from "./components/Alert";
+import "./index.css";
 
 function App() {
-  const [name, setName] = useState(" ");
+  const [name, setName] = useState("");
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: " " });
-
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+    } else if (name && isEditing) {
+    } else {
+      const newItem = { id: new Date().getTime(), title: name };
+      setList([...list, newItem]);
+      setName("");
+      console.log(newItem.id);
+    }
+  };
   return (
     <div className="section-center">
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">Udalit</button>
-      </div>
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Belgilanganlar qatori</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="belgilash joyi"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "Edit" : "Qo'shish"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">Udalit</button>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,24 @@
-const List = () => {
-  return <div>List component</div>;
+const List = ({ items }) => {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button type="button" className="edit-btn">
+                E
+              </button>
+              <button type="button" className="delete-btn">
+                D
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default List;
